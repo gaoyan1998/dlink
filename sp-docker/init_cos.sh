a554a682394d3fdc8f1818ad6dcf5452d7b0f4b5
ikiler <ikiler@126.com>
2024-03-31 15:01:37 +0800

    修改dockerfile 分阶段提交

--- sp-docker/init_cos.sh
+++ sp-docker/init_cos.sh
@@ -3,6 +3,6 @@ if [ ! $USE_COS ];then
 	echo "USE_COS NOT ENABLE!!!!!"
 else
 	echo "USE_COS = $USE_COS !!!!!!!!!!!!!!"
-	coscli cp init_cos.sh cos://flink-checkpoint-1251517753/data/flink-output/${JOB_NAME}/${POD_NAME}/
+	coscli cp /init_cos.sh cos://flink-checkpoint-1251517753/data/flink-output/${JOB_NAME}/${POD_NAME}/
 	/opt/goosefs-lite-1.0.4/bin/goosefs-lite mount  /tmp cosn://flink-checkpoint-1251517753/data/flink-output/${JOB_NAME}/${POD_NAME}
 fi
