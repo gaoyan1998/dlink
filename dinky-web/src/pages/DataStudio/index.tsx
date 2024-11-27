9cacf2a67a52784c8f3a622b38d38de87f9385cd
gaoyan1998 <gaoyan1998@users.noreply.github.com>
2024-11-27 09:04:27 +0000

    Spotless Apply

--- dinky-web/src/pages/DataStudio/index.tsx
+++ dinky-web/src/pages/DataStudio/index.tsx
@@ -579,9 +579,11 @@ const DataStudio: React.FC = (props: any) => {
                     dropMode={'edge'}
                     style={{ position: 'absolute', left: 0, top: 0, right: 0, bottom: 0 }}
                     onLayoutChange={async (newLayout, currentTabId, direction) => {
-                      if (currentTabId && direction=='active'){
-                        const tableData = (dataStudioState.centerContent.tabs as CenterTab[]).find((x) => x.id === currentTabId);
-                        if (tableData){
+                      if (currentTabId && direction == 'active') {
+                        const tableData = (dataStudioState.centerContent.tabs as CenterTab[]).find(
+                          (x) => x.id === currentTabId
+                        );
+                        if (tableData) {
                           const key = Number(currentTabId.replace('project_', ''));
                           updateAction({
                             actionType: DataStudioActionType.TASK_TAB_CHANGE,
